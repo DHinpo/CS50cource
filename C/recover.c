7a59b0a034b02c783d9b361ea84da686f91d3586
Maksym <48384337+DHinpo@users.noreply.github.com>
2019-08-27 19:13:42 +0300

    відновлює jpeg файли

--- C/recover.c
+++ C/recover.c
@@ -22,28 +22,26 @@ if (!rawFile)
     unsigned char jpgID = 0;
     unsigned char buf[BUF_SIZE];
 
-    while (fread(buf, BUF_SIZE, 1, rawFile)) {
-        if (buf[0] == 0xFF && buf[1] == 0xD8 && buf[2] == 0xFF) {
-            if (jpgFile)
-                fclose(jpgFile);
-
-            sprintf(jpgName, "%03d.jpg", jpgID);
-            jpgID++;
-
-            jpgFile = fopen(jpgName, "w");
-            if (!jpgFile) {
-                printf("Could not create \"%s\".\n", jpgName);
-                fclose(rawFile);
-                return 2;
+    while (fread(buf, BUF_SIZE, 1, rawFile)) 
+    {
+        if (buf[0] == 0xFF && buf[1] == 0xD8 && buf[2] == 0xFF)
+            {
+                if (jpgFile)
+                    fclose(jpgFile);
+                    sprintf(jpgName, "%03d.jpg", jpgID);
+                    jpgID++;
+                    jpgFile = fopen(jpgName, "w");
+                        if (!jpgFile) 
+                            {
+                                printf("Could not create \"%s\".\n", jpgName);
+                                fclose(rawFile);
+                                return 2;
+                            }
             }
-        }
-
         if (jpgFile)
             fwrite(buf, BUF_SIZE, 1, jpgFile);
     }
-
     fclose(jpgFile);
     fclose(rawFile);
     return 0;
-
 }
